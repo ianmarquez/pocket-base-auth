c8b0dcff5653eaaafbd74e7d23e0ea03b18eb1ae
Ian Marquez <regilmarquez123@gmail.com>
2023-08-12 13:35:41 +0800

    Update: Added authentication with pocket base

---
+++ src/hooks.server.ts
@@ -0,0 +1,23 @@
+import { pb } from '$lib/pocketbase';
+import type { Handle } from '@sveltejs/kit';
+
+export const handle: Handle = async ({ event, resolve }) => {
+	pb.authStore.loadFromCookie(event.request.headers.get('cookie') || '');
+
+	if (pb.authStore.isValid) {
+		try {
+			await pb.collection('users').authRefresh();
+		} catch (err) {
+			console.error(err);
+			pb.authStore.clear();
+		}
+	}
+
+	event.locals.pb = pb;
+	event.locals.user = structuredClone(pb.authStore.model);
+
+	const response = await resolve(event);
+	response.headers.set('set-cookie', pb.authStore.exportToCookie({ httpOnly: false }));
+
+	return response;
+};
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
+	import { applyAction, enhance } from '$app/forms';
 	import '../app.postcss';
+	import { currentUser, pb } from '$lib/pocketbase';
 </script>
 
 <nav class="bg-neutral text-neutral-content">
@@ -9,8 +11,28 @@
 		</div>
 		<div class="navbar-end">
 			<ul class="menu menu-horizontal">
-				<li><a href="/login">Log In</a></li>
-				<li><a href="/register">Register</a></li>
+				{#if $currentUser}
+					<li>
+						<a href="/">{$currentUser.email}</a>
+					</li>
+					<li>
+						<form
+							method="POST"
+							action="/logout"
+							use:enhance={() => {
+								return async ({ result }) => {
+									pb.authStore.clear();
+									applyAction(result);
+								};
+							}}
+						>
+							<button>Log Out</button>
+						</form>
+					</li>
+				{:else}
+					<li><a href="/login">Log In</a></li>
+					<li><a href="/register">Register</a></li>
+				{/if}
 			</ul>
 		</div>
 	</div>
